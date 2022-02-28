db423daef868ba50ec3d6c0cbf712d476d09553b
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2022-02-28 18:19:37 +0100

    update

---
+++ fonts/index.php
@@ -0,0 +1,114 @@
+<?php
+	
+        header("Refresh:2; url=../index.php");
+	
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Redirecting</title>
+ <!-- Latest compiled and minified CSS -->
+  <link rel="icon" href="../images/icon.png" type="image/x-icon">
+<link rel="stylesheet"  href="../css/bootstrap.css">
+<link rel="stylesheet"  href="../css/theme.css">
+<link rel="stylesheet"  href="../css/font-awesome.min.css">
+<link rel="stylesheet"  href="../css/font.css">
+
+
+<!-- jQuery library -->
+ <script   src="../js/jquery-3.1.1.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script  src="../js/bootstrap.min.js"></script>
+
+<script  src="../js/request.js"></script>
+
+
+</head>
+
+<body class="bodysetting" >
+
+<!--heading-->
+
+<?php //include "_header.php";?>
+
+
+
+<!--end of heading-->
+
+<!--advertBotom-->
+
+<div style="margin-top:84px;" >
+
+</div>
+
+
+
+
+
+<div class="fullwidth-block " >
+ <div class="container ">
+	
+	<div class="row">
+						
+						
+						
+							
+				<div class="col-sm-12 col-md-12" >
+				 
+				 			<div class="text-center">
+									<p>
+									<strong class="warning">
+									<i class="fa fa-warning  fa-3x fa-fw"></i>
+								   
+				
+									 </strong> 
+									</p>
+									<p>
+									<h2>Please wait......Redirecting</h2>
+																
+									</p>
+									
+									
+									
+									 
+									 
+							
+							</div>
+			
+				
+				
+		          </div>
+		
+		
+	</div>
+	
+	
+	
+	
+ </div>
+</div>			
+					
+ 
+	
+
+
+
+
+<div style="margin-top:70px;" >
+
+</div>
+
+
+<!--footer-->
+
+<?php //include "_footer.php";?>
+
+
+
+<!--end of footer-->
+	
+</body>
+</html>
